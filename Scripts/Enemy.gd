005b111e37dc11d9d63ded22e3b70265ce1fd302
Atif Kekeç <kekecatf@outlook.com>
2024-07-09 16:07:12 +0300

    day2

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,18 +1,8 @@
-extends CharacterBody2D
+extends char
+@onready var enemy_sprite = $AnimatedSprite2D
 
-@onready var sprite = $AnimatedSprite2D
-
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-var ziplandi = false
-var dusuluyor = false
-var vuruyormu = false
-var hareket_edebilirmi = true
 
 func _physics_process(delta):
-	var hedef_konum
 	
 	if Input.is_action_just_pressed("Attack"):
 		vuruyormu = true
@@ -27,12 +17,12 @@ func _physics_process(delta):
 
 	if velocity.y > 0 and not is_on_floor() and not vuruyormu:
 		if not dusuluyor:
-			sprite.play("Fall")
+			enemy_sprite.play("Fall")
 			dusuluyor = true
 			print("Dusuluyor")
 
 	if velocity.y == 0 and is_on_floor() and dusuluyor and not vuruyormu:
-		sprite.play("Fall_On_Ground")
+		enemy_sprite.play("Fall_On_Ground")
 		dusuluyor = false
 		print("dusuldu")
 
@@ -40,24 +30,24 @@ func _physics_process(delta):
 
 	if direction != 0 and hareket_edebilirmi and not vuruyormu:
 		if Input.is_action_pressed("Run"):
-			sprite.play("Run")
+			enemy_sprite.play("Run")
 			velocity.x = direction * SPEED * 1.5
 			print("yatay hiz ", velocity.x)
 		else:
-			sprite.play("Walk")
+			enemy_sprite.play("Walk")
 			velocity.x = direction * SPEED
 			print("Yön ", direction)
 		if direction == 1:
-			sprite.flip_h = false
+			enemy_sprite.flip_h = false
 		else:
-			sprite.flip_h = true
+			enemy_sprite.flip_h = true
 	elif direction == 0:
 		velocity.x = lerp(velocity.x, 0.0, 10 * delta)
 		if abs(velocity.x) < 0.001:  # Hız çok küçükse sıfırla
 			velocity.x = 0
 		print("yatay hiz ", velocity.x)
 		if hareket_edebilirmi and not vuruyormu:
-			sprite.play("Idle")
+			enemy_sprite.play("Idle")
 	if not vuruyormu:
 		move_and_slide()
 
@@ -65,16 +55,16 @@ func play_random_attack_animation():
 	var attack_animations = ["Attack1", "Attack2", "Attack3"]
 	var random_index = randi() % attack_animations.size()
 	var random_animation = attack_animations[random_index]
-	sprite.play(random_animation)
+	enemy_sprite.play(random_animation)
 
 func _on_animated_sprite_2d_animation_finished():
-	print("Animation finished: ", sprite.animation)
+	print("Animation finished: ", enemy_sprite.animation)
 	
-	if sprite.animation == "Attack1" or sprite.animation == "Attack2" or sprite.animation == "Attack3":
+	if enemy_sprite.animation == "Attack1" or enemy_sprite.animation == "Attack2" or enemy_sprite.animation == "Attack3":
 		hareket_edebilirmi = true
 		vuruyormu = false
 		
 		
-	if sprite.animation == "Fall_On_Ground":
+	if enemy_sprite.animation == "Fall_On_Ground":
 		dusuluyor = false
 		hareket_edebilirmi = true
